08f490f9959ac5e4ea441920430b3c4dea410304
bikuta6 <vramososuna@gmail.com>
2024-10-23 12:35:53 +0200

    trainer class

--- datagen.py
+++ datagen.py
@@ -1,8 +1,5 @@
 from sim import NBodySimulation, generateDisk3Dv3
 import numpy as np
-import json
-import glob
-import os
 import tqdm
 
 def gen_params():
@@ -19,58 +16,8 @@ def gen_params():
         'angle': [float(np.random.uniform(-1, 1)*2*np.pi), float(np.random.uniform(-1, 1)*2*np.pi), float(np.random.uniform(-1, 1)*2*np.pi)]
     }
 
+
 def generate_scene_2gals():
-    params1 = gen_params()
-    params2 = gen_params()
-
-    particles1 = generateDisk3Dv3(**params1)
-    particles2 = generateDisk3Dv3(**params2)
-
-    t_end = 10.0
-    dt = 0.01
-    softening = 0.1
-    G = 1.0
-
-    particles = particles1 + particles2
-    sim = NBodySimulation(particles, G, softening, dt)
-
-    pos, vel, acc, KE, PE, _, masses, types = sim.run(t_end=t_end, save_states=True)
-    
-    # Convert all arrays to lists
-    pos = np.array(pos).transpose(2, 0, 1)
-    vel = np.array(vel).transpose(2, 0, 1)
-    acc = np.array(acc).transpose(2, 0, 1)
-    KE = KE.flatten().astype(float).tolist()  # Ensure floats
-    PE = PE.flatten().astype(float).tolist()  # Ensure floats
-    masses = masses.flatten().astype(float).tolist()  # Ensure floats
-
-
-    frames = []
-    for i in range(len(pos)):
-        frames.append({
-            'frame': int(i),  # Ensure the frame index is an int
-            'pos': pos[i].tolist(),
-            'vel': vel[i].tolist(),
-            'acc': acc[i].tolist()
-        })
-
-    final_json = {
-        'galaxy1_params': params1,
-        'galaxy2_params': params2,
-        'dt': float(dt),
-        'softening': float(softening),
-        'G': float(G),
-        't_end': float(t_end),
-        'masses': [float(m) for m in masses],
-        'types': types,
-        'KE': KE,
-        'PE': PE,
-        'frames': frames
-    }
-
-    return json.dumps(final_json, indent=4)
-
-def generate_scene_2gals_memory():
     params1 = gen_params()
     params2 = gen_params()
 
@@ -122,54 +69,15 @@ def generate_scene_2gals_memory():
     return final_json
 
 
-def generate_dataset(n_scenes=5, window_size=3, shuffle=True, dir='./train/', save=True):
-    # Ensure the directory exists
-    os.makedirs(dir, exist_ok=True)
-    
-    # the objective is to generate samples from n scenes, of 3 frames each, saving positions, velocities and accelerations, the idea is to predict acceleration from the first frame for the next 2 frames to integrate position and velocity
-    other_files = glob.glob(dir + '*.json')
-    # get the last id from the files, given the structure of the file names is train_0.json, train_1.json, etc
-    last_id = -1
-    for file in other_files:
-        last_id = max(last_id, int(file.split('_')[-1].split('.')[0]))
 
-    new_id = last_id + 1
-    name = dir + f'{dir[1:-1]}_{new_id}.json'
-    
-    dataset = []
-    print(f'Generating dataset with {n_scenes} scenes...')
-    for i in tqdm.tqdm(range(n_scenes)):
-        scene = generate_scene_2gals()
-        scene = json.loads(scene)
-        frames = scene['frames']
-        masses = scene['masses']
-        for j in range(5, len(frames)-window_size):
-            sample = {
-                'masses': masses,
-                'pos': frames[j]['pos'],
-                'vel': frames[j]['vel'],
-                'acc': frames[j]['acc']
-            }
-            for k in range(1, window_size):
-                sample['pos_next{}'.format(k)] = frames[j+k]['pos']
-                sample['vel_next{}'.format(k)] = frames[j+k]['vel']
-            dataset.append(sample)
-    if shuffle:
-        np.random.shuffle(dataset)
-    if save:
-        with open(name, 'w') as f:
-            json.dump(dataset, f)
-
-    return dataset
-
-def generate_dataset_memory(n_scenes=5, window_size = 2, shuffle=True):
+def generate_dataset(n_scenes=5, window_size = 2, shuffle=True):
     # Ensure the directory exists
 
     dataset = []
     
     print(f'Generating dataset with {n_scenes} scenes...')
     for _ in tqdm.tqdm(range(n_scenes)):
-        scene = generate_scene_2gals_memory()
+        scene = generate_scene_2gals()
         types= np.array(scene['types'])
         bh_index = np.where(types == "black hole")[0]
         frames = scene['frames']
@@ -192,29 +100,4 @@ def generate_dataset_memory(n_scenes=5, window_size = 2, shuffle=True):
 
     return dataset
 
-def generate_dataset_past_pos(n_scenes=5, past_pos=5, shuffle=True):
-    # Ensure the directory exists
-
-    dataset = []
-    
-    print(f'Generating dataset with {n_scenes} scenes...')
-    for _ in tqdm.tqdm(range(n_scenes)):
-        scene = generate_scene_2gals_memory()
-        frames = scene['frames']
-        masses = scene['masses']
-        for j in range(past_pos, len(frames)):
-            sample = {
-                'masses': masses,
-                'pos': frames[j]['pos'],
-                'vel': frames[j]['vel'],
-                'acc': frames[j]['acc']
-            }
-            for k in range(1, past_pos+1):
-                sample['pos_past{}'.format(k)] = frames[j-k]['pos']
-
-            dataset.append(sample)
-    if shuffle:
-        np.random.shuffle(dataset)
-
-    return dataset
 
---
+++ trainer.py
@@ -0,0 +1,89 @@
+import torch
+import tqdm
+from datagen import generate_dataset
+import os
+
+
+class Trainer:
+
+    def __init__(self,loss_fn, batch_size, device='cuda', scheduler=None):
+        self.loss_fn = loss_fn
+        self.device = device
+        self.scheduler = scheduler
+        self.batch_size = batch_size
+
+    def get_new_pos_vel(self, acc, pos, vel, dt=0.01):
+        new_vel = vel + acc * dt 
+        new_pos = pos + new_vel * dt
+        return new_pos, new_vel
+
+
+    def train_round(self, data, model, optimizer):
+
+        self.model.train()
+
+        num_batches = len(data) // self.batch_size
+        all_losses = []
+        all_dists = []
+        for i in tqdm.tqdm(range(num_batches)):
+            batch = data[i*self.batch_size:(i+1)*self.batch_size]
+            m = torch.tensor([b['masses'] for b in batch], dtype=torch.float32).to(self.device)
+            pos0 = torch.tensor([b['pos'] for b in batch], dtype=torch.float32).to(self.device)
+            vel0 = torch.tensor([b['vel'] for b in batch], dtype=torch.float32).to(self.device)
+            acc0 = torch.tensor([b['acc'] for b in batch], dtype=torch.float32).to(self.device)
+
+            optimizer.zero_grad()
+            losses = []
+            for j in range(len(batch)):
+                l = 0
+                sample_masses = m[j].unsqueeze(1)
+                sample_pos0 = pos0[j]
+                sample_vel0 = vel0[j]
+                sample_acc0 = acc0[j]
+
+
+                pr_acc0 = model(sample_pos0, sample_vel0, sample_masses)
+                l+= self.loss_fn(sample_acc0, pr_acc0)
+
+
+                losses.append(l)
+
+            total_loss = 64 * sum(losses) / len(batch)
+            all_losses.append(total_loss.item())
+            total_loss.backward()
+
+            optimizer.step()
+
+        print(f'Train Loss: {sum(all_losses)/len(all_losses)}, Train L2: {sum(all_dists)/len(all_dists)}')
+
+    def train(self, model, optimizer, rounds, epochs_per_dataset, scenes_per_dataset=25, weights_dir='./models/'):
+
+        model.to(self.device)
+        if weights_dir is not None:
+            weight_paths = os.listdir(weights_dir)
+            weight_paths.sort(key=lambda x: int(x.split('_')[1].split('.')[0]))
+            try:
+                model.load_state_dict(torch.load(os.path.join(weights_dir, weight_paths[-1])))
+                print(f'Loaded weights from {weight_paths[-1]}')
+                last_model = int(weight_paths[-1].split('_')[1].split('.')[0])
+                last_model += 1
+            except:
+                last_model = 0
+
+        for i in range(rounds):
+            print(f'Round {i}')
+            data = generate_dataset(scenes_per_dataset)
+            for j in range(epochs_per_dataset):
+                print(f'Epoch {j}')
+                self.train_round(data, model, optimizer)
+                if self.scheduler:
+                    self.scheduler.step()
+
+            if weights_dir is not None:
+                torch.save(model.state_dict(), os.path.join(weights_dir, f'model_{last_model}.pt'))
+                print(f'Saved weights to {os.path.join(weights_dir, f"model_{last_model}.pt")}')
+                last_model += 1
+
+        return model
+
+        
